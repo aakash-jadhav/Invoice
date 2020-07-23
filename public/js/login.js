d59b98dbe3ca9c55dcc98d10a11c20fbff1143a9
aakash-jadhav <aakash2jadhav@gmail.com>
2020-07-23 14:11:29 +0530

    Google sign in implemented

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(".modal").modal();
 });
-console.log("Ready v5");
+console.log("Ready v6");
 const auth = firebase.auth();
 const lemail = document.getElementById("email");
 const lpass = document.getElementById("pass");
@@ -23,11 +23,11 @@ function loginFunction() {
   console.log("login button clicked");
   const promise = auth.signInWithEmailAndPassword(lemail.value, lpass.value);
   promise.then(() => {
-    M.toast({ html: "Signing in " });
+    M.toast({ html: "Signing in ", classes: "rounded" });
   });
   promise.catch((e) => {
     console.log(e.message);
-    M.toast({ html: "Something went wrong" });
+    M.toast({ html: e.code, classes: "rounded" });
   });
 }
 
@@ -36,11 +36,11 @@ function forgotPassword() {
   auth
     .sendPasswordResetEmail(femail.value)
     .then(() => {
-      M.toast({ html: "Email sent successfully " });
+      M.toast({ html: "Email sent successfully ", classes: "rounded" });
       console.log("Email sent successfully");
     })
     .catch((e) => {
-      M.toast({ html: e.message });
+      M.toast({ html: e.code, classes: "rounded" });
       console.log(e.message);
     });
 }
@@ -55,16 +55,19 @@ function createAccount() {
         cPass.value
       );
       promise.then((result) => {
-        M.toast({ html: "Account created " });
+        M.toast({ html: "Account created ", classes: "rounded" });
         let user = firebase.auth().currentUser;
         user
           .sendEmailVerification()
           .then(() => {
-            M.toast({ html: "Email sent for verification" });
+            M.toast({
+              html: "Email sent for verification",
+              classes: "rounded",
+            });
             console.log("Verify your email");
           })
           .catch((e) => {
-            console.log(e.message);
+            console.log(e.code);
           });
 
         return result.user.updateProfile({
@@ -72,16 +75,25 @@ function createAccount() {
         });
       });
       promise.catch((e) => {
-        console.log(e.message);
-        M.toast({ html: "Cannot create account" });
+        console.log(e.code);
+        M.toast({ html: "Cannot create account", classes: "rounded" });
       });
       //LOGIC
     } else {
       console.log("Passwords must be same");
     }
   } else {
-    M.toast({ html: "Values cannot be empty" });
+    M.toast({ html: "Values cannot be empty", classes: "rounded" });
   }
+}
+
+//Sign in with google
+function googleSignin() {
+  let provider = new firebase.auth.GoogleAuthProvider();
+  const promise = firebase.auth().signInWithPopup(provider);
+  promise.catch((e) => {
+    console.log(e.code);
+  });
 }
 
 //realtime listner
@@ -91,7 +103,7 @@ firebase.auth().onAuthStateChanged((firebaseUser) => {
     if (firebaseUser.emailVerified) {
       window.location.href = "home.html";
     } else {
-      M.toast({ html: "Email address not verified" });
+      M.toast({ html: "Email address not verified", classes: "rounded" });
       firebase.auth().signOut();
     }
   } else {
