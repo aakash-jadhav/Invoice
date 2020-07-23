852f57a9e17bbf294a37b31c12e8641980105e66
aakash-jadhav <aakash2jadhav@gmail.com>
2020-07-23 13:03:41 +0530

    Sign up functionality implemented along with email verification

--- public/js/home.js
+++ public/js/home.js
@@ -7,7 +7,14 @@ function logout() {
 
 firebase.auth().onAuthStateChanged((firebaseUser) => {
   if (firebaseUser) {
+    console.log("Checking user");
     console.log(firebaseUser);
+    const verify = firebaseUser.emailVerified;
+    console.log(verify);
+    if (!verify) {
+      firebase.auth().signOut();
+      MSAssertion.toast({ html: "Email address is not verified" });
+    }
   } else {
     window.location.href = "login.html";
     console.log("No user");
--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,23 @@
 $(document).ready(function () {
   $(".modal").modal();
 });
-console.log("Ready v2");
+console.log("Ready v5");
 const auth = firebase.auth();
 const lemail = document.getElementById("email");
 const lpass = document.getElementById("pass");
+const femail = document.getElementById("f-pass");
+const fpassword = document.getElementById("remail");
+const activeEmail = document.getElementById("active-label");
+
+const sPass = document.getElementById("s-pass");
+const cPass = document.getElementById("c-pass");
+const sEmail = document.getElementById("s-email");
+const sName = document.getElementById("s-name");
+
+fpassword.addEventListener("click", (e) => {
+  femail.value = lemail.value;
+  activeEmail.classList.add("active");
+});
 
 function loginFunction() {
   console.log("login button clicked");
@@ -17,15 +30,70 @@ function loginFunction() {
     M.toast({ html: "Something went wrong" });
   });
 }
+
+function forgotPassword() {
+  console.log("Forget password");
+  auth
+    .sendPasswordResetEmail(femail.value)
+    .then(() => {
+      M.toast({ html: "Email sent successfully " });
+      console.log("Email sent successfully");
+    })
+    .catch((e) => {
+      M.toast({ html: e.message });
+      console.log(e.message);
+    });
+}
+
 function createAccount() {
   console.log("create account button clicked");
+  if (sPass.value && sEmail.value && cPass.value && sName.value) {
+    if (sPass.value === cPass.value) {
+      console.log("The two passwords are same");
+      const promise = auth.createUserWithEmailAndPassword(
+        sEmail.value,
+        cPass.value
+      );
+      promise.then((result) => {
+        M.toast({ html: "Account created " });
+        let user = firebase.auth().currentUser;
+        user
+          .sendEmailVerification()
+          .then(() => {
+            M.toast({ html: "Email sent for verification" });
+            console.log("Verify your email");
+          })
+          .catch((e) => {
+            console.log(e.message);
+          });
+
+        return result.user.updateProfile({
+          displayName: sName.value,
+        });
+      });
+      promise.catch((e) => {
+        console.log(e.message);
+        M.toast({ html: "Cannot create account" });
+      });
+      //LOGIC
+    } else {
+      console.log("Passwords must be same");
+    }
+  } else {
+    M.toast({ html: "Values cannot be empty" });
+  }
 }
 
 //realtime listner
 firebase.auth().onAuthStateChanged((firebaseUser) => {
   if (firebaseUser) {
     console.log(firebaseUser);
-    window.location.href = "home.html";
+    if (firebaseUser.emailVerified) {
+      window.location.href = "home.html";
+    } else {
+      M.toast({ html: "Email address not verified" });
+      firebase.auth().signOut();
+    }
   } else {
     console.log("not logged in");
   }
