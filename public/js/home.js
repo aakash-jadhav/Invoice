39d7d8531dc069d4e099033306aff2dcde682c1c
Aakash Jadhav <aakash2jadhav@gmail.com>
2020-07-31 19:32:29 +0530

    Cancel button functionality updated
    Cancel button clears the text from input-field and removes additional input field created via add more button

--- public/js/home.js
+++ public/js/home.js
@@ -25,3 +25,50 @@ firebase.auth().onAuthStateChanged((firebaseUser) => {
     console.log("No user");
   }
 });
+
+//TODO:Add more button functionality
+function addMore() {
+  console.log("Add more button clicked");
+  const div = document.getElementById("products");
+  var html = `<div class="divider extra"></div>
+  <div id="products" class = "extra">
+    <div class="row">
+      <div class="input-field col s12 m4">
+        <input
+          type="text"
+          class="product"
+          placeholder="Product/Service"
+        />
+      </div>
+      <div class="input-field col s12 m4">
+        <input type="number" class="quantity" placeholder="Quantity" />
+      </div>
+      <div class="input-field col s12 m4">
+        <input type="number" class="cost" placeholder="Cost" />
+      </div>
+    </div>
+  </div>`;
+
+  var wrapper = document.createElement("div");
+  wrapper.innerHTML = html;
+  div.appendChild(wrapper);
+}
+
+//change total amount on enter of value
+let total = 0;
+$(".cost").keyup(function () {
+  total += $this.val();
+  $("span#totalAmt").text(total);
+});
+// $(".cost").on("change keyup", function (e) {
+//   $("#totalAmt").text(e.value);
+// });
+
+function save() {
+  console.log("Save button clicked");
+}
+
+function cancel() {
+  $("input").val("");
+  $(".extra").remove();
+}
