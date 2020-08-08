494cffdcf539b32ae535948908893b72527264c5
Aakash Jadhav <aakash2jadhav@gmail.com>
2020-08-09 00:07:29 +0530

    Edit feature implemented

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  console.log("V10");
+
   $(".modal").modal({
     onOpenStart: function () {
       $("#invoiceNo").val(Math.floor(Math.random() * 1000000 + 1));
@@ -51,16 +53,20 @@ function fetchData() {
         $(".progressbar").remove();
       } else if (change.type == "removed") {
         let id = change.doc.id;
-
         $(`#${id}`).remove();
+      } else if (change.type == "modified") {
+        let id = change.doc.id;
+        $(`#${id}`).remove();
+        display(change.doc.data());
       }
     });
   });
 }
-//TODO:Add more button functionality
+//used in create new invoice modal
 function addMore() {
+  console.log("Add more function");
   const div = document.getElementById("products");
-  var html = `<div class="divider extra"></div>
+  var html = `
   <div id="products" class = "extra">
     <div class="row">
       <div class="input-field col s12 m4">
@@ -83,6 +89,33 @@ function addMore() {
   wrapper.innerHTML = html;
   div.appendChild(wrapper);
 }
+//used in edit modal
+function add() {
+  console.log("Add more function");
+  let div = document.getElementById("addProducts");
+  var html = `
+  <div id="products" class = "extra">
+    <div class="row">
+      <div class="input-field col s12 m4">
+        <input
+          type="text"
+          class="eproduct"
+          placeholder="Product/Service"
+        />
+      </div>
+      <div class="input-field col s12 m4">
+        <input type="number" class="equantity" placeholder="Quantity" />
+      </div>
+      <div class="input-field col s12 m4">
+        <input type="number" class="ecost" placeholder="Cost" onkeyup="calculateCost()" />
+      </div>
+    </div>
+  </div>`;
+
+  let wrapper = document.createElement("div");
+  wrapper.innerHTML = html;
+  div.appendChild(wrapper);
+}
 
 //change total amount on enter of value
 function calculateCost() {
@@ -91,8 +124,12 @@ function calculateCost() {
   $(".cost").each(function () {
     total += Number($(this).val());
   });
-
   $("#totalAmt").text(total);
+
+  $(".ecost").each(function () {
+    total += Number($(this).val());
+  });
+  $(".totalAmt").text(total);
 }
 
 function save() {
@@ -170,10 +207,14 @@ function display(obj) {
 
   <p>
     <a
-      class="btn-floating blue darken-1 hoverable waves-effect waves-light round"
+    href="#editModal"
+    id = ${obj.invoiceNumber}
+    onclick = "change(id)"
+      class="btn-floating blue darken-1 hoverable waves-effect waves-light round modal-trigger"
       ><i class="material-icons">edit</i>Edit</a
     >
     <a 
+      
       id = ${obj.invoiceNumber}
       onclick = "remove(id)"
       class="btn-floating red darken-1 hoverable waves-effect waves-light right"
@@ -189,3 +230,106 @@ function remove(id) {
   db.collection(emailId).doc(id).delete();
 }
 function search() {}
+
+//edit
+function change(id) {
+  console.log("change function");
+  let docRef = db.collection(emailId).doc(id);
+  docRef
+    .get()
+    .then(function (doc) {
+      if (doc.exists) {
+        console.log(JSON.stringify(doc.data()));
+        edit(doc.data());
+      } else {
+        console.log("No such document");
+      }
+    })
+    .catch(function (e) {
+      console.log("Error getting the document", e);
+    });
+}
+
+function edit(obj) {
+  console.log("edit function");
+  $("h5").text(obj.clientName);
+  $(".iNumber").val(obj.invoiceNumber);
+  $(".issueDate").val(obj.issueDate);
+  $(".dueDate").val(obj.dueDate);
+  $(".clientName").val(obj.clientName);
+  $(".contactNumber").val(obj.contactNumber);
+  $(".email").val(obj.email);
+  let eProducts = "";
+  for (x of obj.products) {
+    eProducts = `<div class="divider extra"></div>
+    <div id="products" class = "extra">
+      <div class="row">
+        <div class="input-field col s12 m4">
+          <input
+            type="text"
+            class="eproduct"
+            placeholder="Product/Service"
+            value = "${x.name}"
+          />
+        </div>
+        <div class="input-field col s12 m4">
+          <input type="number" class="equantity" placeholder="Quantity" 
+          value="${x.quantity}"/>
+        </div>
+        <div class="input-field col s12 m4">
+          <input type="number" class="ecost" placeholder="Cost" onkeyup="calculateCost()"
+          value="${x.cost}" />
+        </div>
+      </div>
+    </div>`;
+    $("#addProducts").append(eProducts);
+  }
+  if (obj.status === "Unpaid") {
+    $(".status").prop("checked", false);
+  } else {
+    $(".status").prop("checked", true);
+  }
+
+  $(".totalAmt").text(obj.totalAmt);
+}
+
+function saveChanges() {
+  let invoice = {
+    invoiceNumber: $(".iNumber").val(),
+    issueDate: $(".issueDate").val(),
+    dueDate: $(".dueDate").val(),
+    clientName: $(".clientName").val(),
+    contactNumber: $(".contactNumber").val(),
+    email: $(".email").val(),
+    products: [],
+
+    status: $(".status").prop("checked") ? "Paid" : "Unpaid",
+    statusColor: $(".status").prop("checked") ? "green-text" : "amber-text",
+
+    totalAmt: $(".totalAmt").text(),
+  };
+
+  for (let i = 0; i < $(" #editModal .eproduct").length; ++i) {
+    let product = {};
+    $(".eproduct").each(function (index) {
+      if (index == i) {
+        product.name = $(this).val();
+      }
+    });
+    $(".equantity").each(function (index) {
+      if (index == i) {
+        product.quantity = $(this).val();
+      }
+    });
+    $(".ecost").each(function (index) {
+      if (index == i) {
+        product.cost = $(this).val();
+      }
+    });
+    // M.toast({ html: product.name });
+    invoice.products.push(product);
+  }
+
+  console.log(JSON.stringify(invoice));
+  firestore(invoice);
+}
